3702ddbc48cb155f787a4814cf91324d3463967a
kwiatkowskaaleksandra <85847869+kwiatkowskaaleksandra@users.noreply.github.com>
2023-03-28 13:47:25 +0200

    first

---
+++ src/main/java/biuropodrozy/gotravel/GotravelApplication.java
@@ -0,0 +1,13 @@
+package biuropodrozy.gotravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/biuropodrozy/gotravel/GotravelApplicationTests.java
@@ -0,0 +1,13 @@
+package biuropodrozy.gotravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
